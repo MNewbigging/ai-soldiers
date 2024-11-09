ea4699b4205b37466aa6c7f941988a7621a9039d
Matt Newbigging <mrnewbigging@gmail.com>
2024-11-09 21:27:39 +0000

    neater loaders

--- src/game/core/asset-manager.ts
+++ src/game/core/asset-manager.ts
@@ -73,56 +73,22 @@ export class AssetManager {
      * won't always work (they'll not find certain bones)
      */
 
-    // american soldier
-    // const asUrl = new URL(
-    //   "/models/Character_American_Soldier_01.fbx",
-    //   import.meta.url
-    // ).href;
-    // fbxLoader.load(asUrl, (group) => {
-    //   this.models.set("soldier-am", group);
-    // });
-
     this.loadModel(fbxLoader, "Character_American_Soldier_01", "soldier-am");
-
-    // american submachine gun
-    const arUrl = new URL(
-      "/models/SM_Wep_American_SubMachineGun_01.fbx",
-      import.meta.url
-    ).href;
-    fbxLoader.load(arUrl, (group) => {
-      this.models.set("smg-am", group);
-    });
-
-    // german soldier
-    const gsUrl = new URL(
-      "/models/Character_German_Soldier_01.fbx",
-      import.meta.url
-    ).href;
-    fbxLoader.load(gsUrl, (group) => {
-      this.models.set("soldier-de", group);
-    });
-
-    // german assault rifle
-    const germanArUrl = new URL(
-      "/models/SM_Wep_German_AssaultRifle_01.fbx",
-      import.meta.url
-    ).href;
-    fbxLoader.load(germanArUrl, (group) => {
-      this.models.set("rifle-de", group);
-    });
+    this.loadModel(fbxLoader, "SM_Wep_American_SubMachineGun_01", "smg-am");
+    this.loadModel(fbxLoader, "Character_German_Soldier_01", "soldier-de");
+    this.loadModel(fbxLoader, "SM_Wep_German_AssaultRifle_01", "rifle-de");
   }
 
   private loadModel(
-    fbxLoader: FBXLoader,
+    loader: FBXLoader,
     filename: string,
     alias: string,
     onLoad?: (group: THREE.Group) => void
   ) {
     const path = `${getPathPrefix()}/models/${filename}.fbx`;
     const url = getUrl(path);
-    console.log("test 3");
 
-    fbxLoader.load(url, (group: THREE.Group) => {
+    loader.load(url, (group: THREE.Group) => {
       onLoad?.(group);
       this.models.set(alias, group);
     });
@@ -132,32 +98,12 @@ export class AssetManager {
     rgbeLoader: RGBELoader,
     textureLoader: THREE.TextureLoader
   ) {
-    // skybox
-    const orchardUrl = new URL(
-      "/textures/orchard_cartoony.hdr",
-      import.meta.url
-    ).href;
-    rgbeLoader.load(orchardUrl, (texture) => {
+    this.loadTexture(rgbeLoader, "orchard_cartoony.hdr", "hdri", (texture) => {
       texture.mapping = THREE.EquirectangularReflectionMapping;
-      this.textures.set("hdri", texture);
     });
 
-    // war texture 1A
-    const a1Url = new URL(
-      "/textures/PolygonWar_Texture_01_A.png",
-      import.meta.url
-    ).href;
-    textureLoader.load(a1Url, (texture) => {
-      texture.colorSpace = THREE.SRGBColorSpace;
-      this.textures.set("war-1A", texture);
-    });
-
-    // prototype floor texture
-    const protoUrl = new URL("/textures/texture_02.png", import.meta.url).href;
-    textureLoader.load(protoUrl, (texture) => {
-      texture.colorSpace = THREE.SRGBColorSpace;
-      this.textures.set("floor", texture);
-    });
+    this.loadTexture(textureLoader, "PolygonWar_Texture_01_A.png", "war-1A");
+    this.loadTexture(textureLoader, "texture_02.png", "floor");
   }
 
   private loadTexture(
@@ -169,29 +115,26 @@ export class AssetManager {
     const path = `${getPathPrefix()}/textures/${filename}`;
     const url = getUrl(path);
     loader.load(url, (texture) => {
+      // All textures need this
+      texture.colorSpace = THREE.SRGBColorSpace;
+
       onLoad?.(texture);
       this.textures.set(alias, texture);
     });
   }
 
   private loadAnimations(fbxLoader: FBXLoader) {
-    // rifle idle
-    const rifleIdleUrl = new URL("/anims/rifle-idle-1.fbx", import.meta.url)
-      .href;
-    fbxLoader.load(rifleIdleUrl, (group) => {
-      if (group.animations.length) {
-        const clip = group.animations[0];
-        clip.name = "rifle-idle-1";
-        this.animations.set(clip.name, clip);
-      }
-    });
+    this.loadAnimation(fbxLoader, "rifle-idle-1", "rifle-idle-1");
+    this.loadAnimation(fbxLoader, "rifle-idle-2", "rifle-idle-2");
+  }
 
-    const rifleIdle2Url = new URL("/anims/rifle-idle-2.fbx", import.meta.url)
-      .href;
-    fbxLoader.load(rifleIdle2Url, (group) => {
+  private loadAnimation(loader: FBXLoader, filename: string, alias: string) {
+    const path = `${getPathPrefix()}/anims/${filename}.fbx`;
+    const url = getUrl(path);
+    loader.load(url, (group) => {
       if (group.animations.length) {
         const clip = group.animations[0];
-        clip.name = "rifle-idle-2";
+        clip.name = alias;
         this.animations.set(clip.name, clip);
       }
     });
